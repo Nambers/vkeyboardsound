db55aea77bde270f40709f7ea0543b47491b7b15
Eritque arcus <1930893235@qq.com>
2021-11-23 10:43:22 +0800

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+import threading
+
+import keyboard
+import pygame.mixer
+
+
+def playSound(s: pygame.mixer.Sound):
+    s.play()
+
+
+def hook(event: keyboard.KeyboardEvent):
+    if event.event_type == "down":
+        press()
+    else:
+        release()
+
+
+def press():
+    global press_sound
+    threading.Thread(target=playSound, args=[press_sound]).start()
+
+
+def release():
+    global release_sound
+    threading.Thread(target=playSound, args=[release_sound]).start()
+
+
+if __name__ == '__main__':
+    pygame.mixer.pre_init()
+    pygame.mixer.init()
+    press_sound = pygame.mixer.Sound("press.mp3")
+    release_sound = pygame.mixer.Sound("release.mp3")
+    keyboard.hook(hook)
+    keyboard.wait()
